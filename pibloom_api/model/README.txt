e0ff7a5f26d69cda8a06c787b11a44b88412c24f
danczw <dev.dc@pm.me>
2022-08-18 18:14:15 +0200

    update to local model loading

---
+++ pibloom_api/model/README.txt
@@ -0,0 +1,9 @@
+download 
+
+- config.json
+- pytorch_model.bin
+- special_tokens_map.json
+- tokenizer.json
+- tokenizer.json
+
+from https://huggingface.co/bigscience/bloom-560m/tree/main
--- pibloom_api/pibloom/model.py
+++ pibloom_api/pibloom/model.py
@@ -1,25 +1,23 @@
-from transformers import BloomModel, BloomTokenizerFast, pipeline
+from transformers import BloomForCausalLM, BloomTokenizerFast, TextGenerationPipeline
 
 class bloom_model:
     def __init__(self):
         self.tokenizer = BloomTokenizerFast.from_pretrained(
-            "bigscience/bloom-560m"
-            , low_cpu_mem_usage=True
+            pretrained_model_name_or_path = './model/'
+            , local_files_only = True
+            , low_cpu_mem_usage = True
         )
         
-        self.model = BloomModel.from_pretrained("bigscience/bloom-560m")
-        
-        # self.generator = pipeline(
-        #     "text-generation"
-        #     , model=self.model
-        #     , tokenizer=self.tokenizer
-        # )
-        
-        self.generator = pipeline(
-            model="bigscience/bloom-560m"
-            , tokenizer=self.tokenizer
+        self.model = BloomForCausalLM.from_pretrained(
+            pretrained_model_name_or_path = './model/'
         )
         
+        self.generator = TextGenerationPipeline(
+            task = 'text-generation',
+            model = self.model,
+            tokenizer = self.tokenizer
+        )
+                        
     def predict(self, content: str) -> str:
         answer = self.generator(content, max_new_tokens=100)
         return answer
