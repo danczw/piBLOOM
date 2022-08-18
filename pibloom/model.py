53234871fd859e1892b8fccb862782644f58fc1d
danczw <dev.dc@pm.me>
2022-08-18 10:46:45 +0200

    add max_new_tokens, refactor for readability

--- pibloom/model.py
+++ pibloom/model.py
@@ -2,11 +2,24 @@ from transformers import BloomModel, BloomTokenizerFast, pipeline
 
 class bloom_model:
     def __init__(self):
-        self.tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-560m")
+        self.tokenizer = BloomTokenizerFast.from_pretrained(
+            "bigscience/bloom-560m"
+            , low_cpu_mem_usage=True
+        )
+        
         self.model = BloomModel.from_pretrained("bigscience/bloom-560m")
-        # self.generator = pipeline("text-generation", model=self.model, tokenizer=self.tokenizer)
-        self.generator = pipeline(model="bigscience/bloom-560m", tokenizer=self.tokenizer)
+        
+        # self.generator = pipeline(
+        #     "text-generation"
+        #     , model=self.model
+        #     , tokenizer=self.tokenizer
+        # )
+        
+        self.generator = pipeline(
+            model="bigscience/bloom-560m"
+            , tokenizer=self.tokenizer
+        )
         
     def predict(self, content: str) -> str:
-        answer = self.generator(content)
+        answer = self.generator(content, max_new_tokens=100)
         return answer
