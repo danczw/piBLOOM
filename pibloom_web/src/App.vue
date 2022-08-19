2867be1f7911408c9b4bafc333e4ac7b56e707e5
danczw <dev.dc@pm.me>
2022-08-19 02:57:49 +0200

    update main app

--- pibloom_web/src/App.vue
+++ pibloom_web/src/App.vue
@@ -5,16 +5,16 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
+    <!-- <img
+      alt="logo"
       class="logo"
       src="@/assets/logo.svg"
       width="125"
       height="125"
-    />
+    /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="piBLOOM" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -42,9 +42,13 @@ nav {
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
+  margin-bottom: 2rem;
 }
 
 nav a.router-link-exact-active {
+  border-bottom: 1px solid white !important;
+  border-bottom-left-radius: 10%;
+  border-bottom-right-radius: 10%;
   color: var(--color-text);
 }
 
