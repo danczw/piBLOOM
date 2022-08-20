9c7a9729208afea8ee0d1062849ba757938aabb0
danczw <dev.dc@pm.me>
2022-08-20 18:33:01 +0200

    add CORS exemptions

--- pibloom_api/pibloom/app.py
+++ pibloom_api/pibloom/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 # handle import via poetry initialization and python
 try:
@@ -7,19 +8,24 @@ except ImportError:
     from model import bloom_model
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}}) # TODO: add origin restriction
+
 bloom = bloom_model()
 
 @app.route('/index/', methods=['GET'])
 def index():
-    return jsonify({'message': 'Hello World!'})
+    response = jsonify({'data': 'Hello World!'})
+        
+    return response
 
 @app.route('/chat/', methods=['POST'])
 def chat_with_bot():
     content = request.json['content']
-    
     answer = bloom.predict(content=content)
 
-    return jsonify({'data': answer[0]['generated_text']})
+    response = jsonify({'data': answer[0]['generated_text']})
+
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
