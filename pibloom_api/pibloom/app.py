8d634482a2917c3f15cd10d5701bf73471f71eea
danczw <dev.dc@pm.me>
2022-08-27 21:00:13 +0200

    update CORS origin and add comments

--- pibloom_api/pibloom/app.py
+++ pibloom_api/pibloom/app.py
@@ -14,19 +14,22 @@ bloom = bloom_model()
 # set request and response models
 class DataIn(BaseModel):
     content: str
-    
+
 class DataOut(BaseModel):
     data: str
 
+# init app
+app = FastAPI()
 
-# set cross-origin resource sharing
+# define CORS allowed origins
 origins = [
     "http://localhost:5050"
     , "http://127.0.0.1:5050"
+    , "http://localhost:8080"
+    , "http://127.0.0.1:8080"
 ]
 
-# init app
-app = FastAPI()
+# set CORS policy
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -35,17 +38,21 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
+# root path request
 @app.get('/', status_code=status.HTTP_200_OK)
 async def root():
     response = {'data': 'Hello World!'}
     return response
 
+# bloom model inference request
 @app.post('/chat/', response_model=DataOut, status_code=status.HTTP_200_OK)
 async def chat_with_bot(data_in: DataIn):
     if data_in.content:
+        # generate BLOOM model response
         answer = bloom.predict(content=data_in.content)
         response = {'data': answer[0]['generated_text']}
     else:
+        # return error if no input
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail='Empty prompt'
